b3ba0bdbf6b0e5a8b8bd6fa0c7ee2c1666247238
Alf-caput <alfre.roble.aba@gmail.com>
2022-12-12 15:32:42 +0100

    Hard changes

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -1,29 +1,47 @@
 """Ejercicio 2
-En un equipo de corredores se quiere guardar el nombre de los corredores que entrenan, y los kilómetros que entrenan 
-cada día de la semana. 
-Se habrá de utilizar dos arreglos: 
-Nombre_Corredor: Lista para guardar los nombres de los corredores. 
-kilometros: Tabla para guardar los kilómetros que realizan cada día de la semana. 
-Se quiere generar una nueva lista (“total_kms”) con los kilómetros totales que entrena cada 
-corredor. 
-Al finalizar se muestra la lista con los nombres de Corredores y los kilómetros que ha realizado. 
+En un equipo de corredores se quiere guardar el nombre de los corredores que entrenan, y los kilómetros que entrenan
+cada día de la semana.
+Se habrá de utilizar dos arreglos:
+Nombre_Corredor: Lista para guardar los nombres de los corredores.
+Kilómetros: Tabla para guardar los kilómetros que realizan cada día de la semana.
+Se quiere generar una nueva lista (“total_kms”) con los kilómetros totales que entrena cada
+corredor.
+Al finalizar se muestra la lista con los nombres de Corredores y los kilómetros que ha realizado.
 """
+import random
+
+
+class Corredor:
+    def __init__(self, nombre, kms):
+        self.nombre = nombre
+        self.kms_dia = dict(zip(["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"], kms))
+
+    # Definimos la propiedad total_kms que nos devuelve la suma de kms del corredor a lo largo de la semana
+    @property
+    def total_kms(self):
+        return sum(self.kms_dia.values())
+
+    # El método __str__ nos permite representar al objeto cuando se pasa a la función print()
+    def __str__(self):
+        return f"{self.nombre} ha realizado {self.total_kms} km en total."
+
 
 def main():
-    while ((num := int(input("¿Cuantos corredores tiene la lista?: "))) < 1 & (dias := int(input("¿Cuántos días a la semana corren? "))) < 1):
-        print("Cantidad de corredores o de días errónea. Por favor repita la operación.")
-    nombre_corredor = [input(f"Introduzca el {i + 1}º corredor: ") for i in range(num)]
-    kilometros_corredor = [int(input(f"Introduzca los km del {i + 1}º corredor el {j+1} día: ")) for j in range(dias) for i in range(num)]
-    kilometros = [[nombre_corredor],[kilometros_corredor]]
-    aux, j = 0, 0
-    total_kms = []
-    while(j < num):
-        for i in range(j,len(kilometros_corredor),num):
-            aux += kilometros_corredor[i]
-        total_kms.append(aux)
-        print(f"{nombre_corredor[j]} - {total_kms[j]} kms")
-        aux = 0
-        j += 1
+    Nombre_Corredor = ["Javier", "Pedro", "Luis", "Pepe", "Alfonso"]
+    # Generamos una lista con listas de 7 elementos que contienen los km que ha realizado cada dia cada corredor
+    kilometros = [[random.randint(0, 4) * 5 for dia in range(7)] for corredor in Nombre_Corredor]
+
+    # Generamos una lista de objetos instanciados de la clase Corredor
+    corredores = [Corredor(nombre, kms) for nombre, kms in zip(Nombre_Corredor, kilometros)]
+
+    # Obtenemos el total de kms de cada corredor con la propiedad total_kms
+    total_kms = [corredor.total_kms for corredor in corredores]
+
+    for corredor in corredores:
+        print(corredor)
+    return
+
 
 if __name__ == '__main__':
     main()
+
