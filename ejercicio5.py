2ebf38ef13b2a29edef7cc759799f26f3b18b5ea
Alf-caput <alfre.roble.aba@gmail.com>
2022-12-13 11:24:04 +0100

    campos

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,75 +1,41 @@
 import tkinter
-from tkinter import ttk
-from tkinter import messagebox
-import os
 import openpyxl
 
 
-window = tkinter.Tk()
-window.title("Introducción de la información del usuario")
-
-frame = tkinter.Frame(window)
-frame.pack()
-
-user_info_frame =tkinter.LabelFrame(frame, text="Información del usuario")
-user_info_frame.grid(row= 0, column=0, padx=20, pady=10)
-
-nombre_etiqueta = tkinter.Label(user_info_frame, text="Nombre")
-nombre_etiqueta.grid(row=0, column=0)
-apellido_etiqueta = tkinter.Label(user_info_frame, text="Apellido")
-apellido_etiqueta.grid(row=0, column=1)
-edad_etiqueta = tkinter.Label(user_info_frame, text="Edad")
-edad_etiqueta.grid(row=2, column=0)
-telefono_etiqueta = tkinter.Label(user_info_frame, text="Telefono")
-telefono_etiqueta.grid(row=2, column=1)
-correo_etiqueta = tkinter.Label(user_info_frame, text="Correo")
-correo_etiqueta.grid(row=4, column=0)
-
-entrada_nombre = tkinter.Entry(user_info_frame)
-entrada_apellido = tkinter.Entry(user_info_frame)
-entrada_edad = tkinter.Entry(user_info_frame)
-entrada_correo = tkinter.Entry(user_info_frame)
-entrada_telefono = tkinter.Entry(user_info_frame)
-entrada_nombre.grid(row=1, column=0)
-entrada_apellido.grid(row=1, column=1)
-entrada_edad.grid(row=3, column=0)
-entrada_telefono.grid(row=3, column=1)
-entrada_correo.grid(row=5, column=0)
+def introducir_info(datos):
+    for campo, dato in datos.items():
+        valor = dato.get()
+        print(f"{campo} = {valor}")
+    return
 
 
-for widget in user_info_frame.winfo_children():
-    widget.grid_configure(padx=10, pady=5)
-    
-def introducirInformacion():
-    nombre = entrada_nombre.get()
-    apellido = entrada_apellido.get()
+def main():
+    window = tkinter.Tk()
+    window.title("Introducción de la información del usuario")
 
-    if nombre and apellido:
-        edad = entrada_edad.get()
-        telefono = entrada_telefono.get()
-        correo = entrada_correo.get()
+    frame = tkinter.Frame(window)
+    frame.pack()
 
-        print("Nombre ", nombre, "Apellido: ", apellido)
-        print("Edad: ", edad)
-        print("Teléfono: ", telefono, "Correo: ", correo)
-        print("------------------------------------------")
+    user_info_frame = tkinter.LabelFrame(frame, text="Información del usuario")
+    user_info_frame.grid(row=0, column=0, padx=20, pady=10)
 
-        filepath = "Users\Rubén\Desktop"
+    variable_names = ["Nombre", "Apellido", "Edad", "Correo", "Teléfono"]
+    datos = dict()
+    for index, variable in enumerate(variable_names):
+        etiqueta = tkinter.Label(user_info_frame, text=variable)
+        etiqueta.grid(row=index, column=0, padx=10, pady=10)
 
-        if not os.path.exists(filepath):
-            workbook = openpyxl.Workbook()
-            sheet = workbook.active
-            heading = ["Nombre", "Apellido", "Edad", "Correo", "Teléfono"]
-            sheet.append(heading)
-            workbook.save(filepath)
-        workbook = openpyxl.load_workbook(filepath)
-        sheet = workbook.active
-        sheet.append([nombre, apellido, edad, correo, telefono])
-        workbook.save(filepath)
+        espacio_escritura = tkinter.Entry(user_info_frame)
+        espacio_escritura.grid(row=index, column=1, padx=10, pady=10)
+
+        datos[variable] = espacio_escritura
+
+    button = tkinter.Button(frame, text="Introducir información", command=lambda: introducir_info(datos))
+    button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+
+    window.mainloop()
+    return
 
 
-# Boton
-button = tkinter.Button(frame, text="Introducir información", command= introducirInformacion)
-button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
